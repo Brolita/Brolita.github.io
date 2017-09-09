bd453995ce13fedcaad6a3be54564eabc73ca2b4
Corey Byrne <coreyrbyrne@gmail.com>
2017-09-09 15:54:48 -0700

    Added simple index.html

---
+++ static/js/replay.js
@@ -0,0 +1,39 @@
+function getReplays() 
+{
+	var pos = 0;
+	var data = "";
+	var req = XMLHttpRequest ? new XMLHttpRequest() :
+							   new ActiveXObject("Microsoft.XMLHTTP");
+	var body = document.getElementsByName('body');
+	req.onreadystatechange = function() 
+	{
+		var liveness = $("#replays-liveness");
+		if (this.readyState > 2) 
+		{
+			var orig_sz = data.length;
+			data       += this.responseText.substring(pos);
+			pos         = this.responseText.length;
+			var sep_pos = data.indexOf("\x1e", orig_sz);
+			while (sep_pos != -1) 
+			{
+				if (sep_pos > 0) 
+				{
+					var msg = JSON.parse(data.substr(0, sep_pos));
+					body.dispatchEvent(new CustomEvent('replay', msg));
+				}
+
+				data    = data.substr(sep_pos + 1);
+				sep_pos = data.indexOf("\x1e");
+			}
+		}
+		if (this.readyState == 4) 
+		{
+			req.abort();
+			getReplays();
+		}
+	};
+	req.open("GET", "http://dustkid.com/backend/events.php", true);
+	req.send(null);
+}
+
+getReplays();
